bcff01e77c1718a4b306fd89dec58bcef68022e7
Kranidos1 <76439725+Kranidos1@users.noreply.github.com>
2022-11-17 20:51:04 +0100

    Prime pagine
    Da migliorare gli inserimenti dei cdn, il loro ordine. Costruita la navbar per l'amministratore. Da inserire cdn di googlechart o di chartjs

---
+++ homepage.php
@@ -0,0 +1,11 @@
+<?php
+include "navbarAMMINISTRATORE.php";
+
+echo' <div class = "container">';
+
+
+echo'</div>';
+
+
+include "coda.php";
+?>
---
+++ navbarAMMINISTRATORE.php
@@ -0,0 +1,41 @@
+<?php
+
+include "head.php";
+
+echo'
+</head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand px-2" href="#">NOME SITO </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item ">
+        <a class="nav-link active" href="#">Registra</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link active" href="#">Modifica Menù</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link active" href="#">Crea Avviso</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link active" href="#">HomePage</a>
+       </li>
+    </ul>
+    <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+      <li class="nav-item px-2"><a class="nav-link active ">
+      <i class="fa-sharp fa-solid fa-user"></i>User: tia</a></li>
+      <li class="nav-item  px-2"><a class="nav-link active"  href="#">
+      <i class="fa-sharp fa-solid fa-right-from-bracket"></i>  LOGOUT</a>
+      </li>
+    </ul>
+    </div>
+  </div>
+  </nav>';
+
+  //.$_SESSION["nome"]
+?>
