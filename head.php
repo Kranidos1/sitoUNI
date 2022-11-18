47754150296217effeae4d91606cd8954863d11c
Kranidos1 <76439725+Kranidos1@users.noreply.github.com>
2022-11-18 11:22:58 +0100

    Stylesheet,javascript
    Aggiustati gli imports css,javascript

--- head.php
+++ head.php
@@ -5,6 +5,15 @@ echo'<!doctype html>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!--per fontawesome icons-->
+    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
+    <zzscript srzzc="http://code.jquery.com/jquery.js"></zzscript>
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    
     <style>
 
         #map {
@@ -24,11 +33,7 @@ echo'<!doctype html>
       }
         
         </style>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-    <script
-    src="https://code.jquery.com/jquery-3.6.1.min.js"
-    integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="
-    crossorigin="anonymous"></script>    ';
+  
+    ';
     
 ?>
